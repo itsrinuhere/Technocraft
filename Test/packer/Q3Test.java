a671210bdc58bbc4f995bd69cb077613a7bc1ae6
poojitha0616 <poojithareddyvontela@gmail.com>
2022-05-26 15:22:55 +0530

    c

--- Test/packer/Q3Test.java
+++ Test/packer/Q3Test.java
@@ -13,5 +13,47 @@ public class Q3Test {
         var q3 = new Q3();
        // assertSame("TEST CASE 1:","ello ",q3.solution("Hello world", 1, 5));
        assertEquals("ello " ,q3.solution("Hello world", 1, 5));
+    }@Test  
+    public void testcase2()throws Exception{
+        var q3 = new Q3();
+       // assertSame("TEST CASE 1:","agdevi ",q3.solution("vaagdevi college", 2, 8));
+       assertEquals("agdevi " ,q3.solution("vaagdevi college", 2, 8));
+    }@Test  
+    public void testcase3()throws Exception{
+        var q3 = new Q3();
+       // assertSame("TEST CASE 1:","java",q3.solution("java lab", 0, 3));
+       assertEquals("java" ,q3.solution("java lab", 0, 3));
+    }@Test  
+    public void testcase4()throws Exception{
+        var q3 = new Q3();
+       // assertSame("TEST CASE 1:","c l",q3.solution("c language", 0, 2));
+       assertEquals("c l" ,q3.solution("c language", 0, 2));
+    }@Test  
+    public void testcase5()throws Exception{
+        var q3 = new Q3();
+       // assertSame("TEST CASE 1:","no_cr",q3.solution("techno_craft", 4, 8));
+       assertEquals("no_cr" ,q3.solution("techno_craft", 4, 8));
+    }@Test  
+    public void testcase6()throws Exception{
+        var q3 = new Q3();
+       // assertSame("TEST CASE 1:","++",q3.solution("c and c++", 7, 8));
+       assertEquals("++" ,q3.solution("c and c++", 7, 8));
+    }@Test  
+    public void testcase7()throws Exception{
+        var q3 = new Q3();
+       assertEquals("SE" ,q3.solution("CSE", 1, 2));
+    }@Test  
+    public void testcase8()throws Exception{
+        var q3 = new Q3();
+       assertEquals("T lab" ,q3.solution("WT lab", 1, 5));
+    }@Test  
+    public void testcase9()throws Exception{
+        var q3 = new Q3();
+       assertEquals("TM" ,q3.solution("HTML", 1, 2));
+    }@Test  
+    public void testcase10()throws Exception{
+        var q3 = new Q3();
+       assertEquals("_Y_T" ,q3.solution("P_Y_THON", 1, 4));
     }
 }
+
--- Test/packer/Q4Test.java
+++ Test/packer/Q4Test.java
@@ -10,5 +10,44 @@ public class Q4Test {
     public void testcase1()throws Exception{
        var q4 = new Q4();
        assertEquals( 6, q4.sum(321)," Test case 1");
+    }@Test
+    public void testcase2()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 11, q4.sum(542)," Test case 2");
+    }@Test
+    public void testcase3()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 9, q4.sum(6012)," Test case 3");
+    }@Test
+    public void testcase4()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 5, q4.sum(104)," Test case 4");
+    }@Test
+    public void testcase5()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 24, q4.sum(987)," Test case 5");
+    }@Test
+    public void testcase6()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 5, q4.sum(500)," Test case 6");
+    }@Test
+    public void testcase7()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 9, q4.sum(153)," Test case 7");
+    }@Test
+    public void testcase8()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 9, q4.sum(9000)," Test case 8");
+    }@Test
+    public void testcase9()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 13, q4.sum(490)," Test case 9");
+    }@Test
+    public void testcase10()throws Exception{
+       var q4 = new Q4();
+       assertEquals( 11, q4.sum(650)," Test case 10");
     }
 }
+
+
+
